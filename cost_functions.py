db08694223e4e51250905f1c2e060ecdaa9e61e3
mnyasin26 <forsvive22@gmail.com>
2022-11-12 22:21:43 +0700

    adding commentar

--- cost_functions.py
+++ cost_functions.py
@@ -1,3 +1,6 @@
+# jadi ada 2 fungsi buat ngitung cost, tapi nantinya mah hasil 2 fungsi ini ditambah
+
+# fungsi untuk menghitung cost dari : adanya tabrakan tiap gen dan soft constraint yg tadi ada di doc, pokoknya tentang urutan apalah, kita ga make
 def cost(chromosome):
     """
     Cost function for all hard constraints and soft constraint regarding preferred order. All parameters are empirical.
@@ -10,21 +13,38 @@ def cost(chromosome):
     subjects_cost = 0
 
     # Traverse all classes for hard constraints
+    # mengecek hard constraint dari kromosom dengan memeriksa tiap gen
     for single_class in chromosome[0]:
-        time = single_class['Assigned_time']
+
+        time = single_class['Assigned_time'] # jam pelajaran
+        # contohnya jam pelajaran ke-0 ya ges, senin pagi
         class_len = single_class['Length']
+        # contohnya 3 jam pelajaran
 
         # Check hard constraint violation in classes time frame
+        # mengecek adanya tabrakan atau tidak
         for i in range(time, time + int(class_len)):
+            # klo dari contoh tadi, berarti perulangan ini bakalan ngecek 3 kali, yaitu di jam pelajaran 0, 1, 2
+
+            # mengecek jadwal guru
             if chromosome[1][single_class['Professor']][i] > 1:
+                # klo lebih dari 1 itu berarti dalam 1 waktu si guru ngajar lebih dari 1 kelas, kan nabrak tuh
+                # berarti tambah costnya
                 prof_cost += 1
+
+            # sama tapi buat ruangan
             if chromosome[2][single_class['Assigned_classroom']][i] > 1:
                 classrooms_cost += 1
+
+            # sama tapi buat kelas (misal C1)
             for group in single_class['Group']:
                 if chromosome[3][group][i] > 1:
                     groups_cost += 1
 
     # Traverse all classes for soft constraint regarding preferred order
+
+    # mengecek soft constraint dari kromosom dengan memeriksa tiap gen
+    # tapi kita kyknya ga make
     for single_class in chromosome[4]:
         for lab in chromosome[4][single_class]['L']:
             for practice in chromosome[4][single_class]['V']:
@@ -46,7 +66,7 @@ def cost(chromosome):
 
     return prof_cost + classrooms_cost + groups_cost + round(subjects_cost, 4)
 
-
+# fungsi untuk menghitung cost dari : idleness kelas (C1) dan guru
 def cost2(chromosome):
     """
     Cost function for all hard constraints and all soft constraints. All parameters are empirical.
--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import json
 import random
 
-
+# fungsi untuk mengambil data buat gen nantinya, dalam kasus ini gen-nya adalah satu KBM
 def load_data(path):
     with open(path, 'r') as read_file:
         data = json.load(read_file)
@@ -10,54 +10,106 @@ def load_data(path):
         classroom = university_class['Classroom']
         university_class['Classroom'] = data['Classroom'][classroom]
 
-    data = data['Casovi']
+    data = data['Casovi']  # sebenernya kita cuma butuh ini aaaaaa
 
     return data
 
-
+# fungsi untuk membuat satu jadwal penuh / satu calon solusi
 def generate_chromosome(data):
-    professors = {}
-    classrooms = {}
-    groups = {}
-    subjects = {}
 
-    new_data = []
+    # kemungkinan variabel2 yg bawah ini buat nandain jadwal masing2
+    professors = {} # jadwal guru ngajar
+    classrooms = {} # jadwal ruangan kepake
+    groups = {} # jadwal kelas masuk
+    subjects = {} # jadwal mapelnya tu kapan dan kelas yg masuknya apa
 
+    new_data = [] # kromosom -> satu jadwal
+
+    # ini kayaknya buat alokasi awal dari variabel2 penentu cost tadi
     for single_class in data:
-        professors[single_class['Professor']] = [0] * 40
+
+        # alokasi penanda jadwal guru, 0 berarti kosong alias gak ngajar
+        # nah si penanda ini harus 0 atau 1, gaboleh lebih
+        # karena dalam 1 jam pelajaran, si guru cuma boleh ngajar di 1 kelas
+        professors[single_class['Professor']] = [0] * 40 # -> dibikin semuanya isi 0 dulu, kyk biasa ges, inisialisasi
+        # nb : sebenernya 40 bisa diganti 38 kyknya ges, tp kan biar jaga2 kita jadiin 40 aja karena 5 hari kali 8 jam pelajaran tiap harinya
+        # contoh
+        # professors['Najma'][0] = 1 -> ini maksudnya di jam pelajaran ke-0 (senin jam 7) itu Najma ada jadwal
+
+        # kita ga make ini jadi abaikan :D tp intinya mah sama kyk di atas
         for classroom in single_class['Classroom']:
             classrooms[classroom] = [0] * 40
         for group in single_class['Group']:
             groups[group] = [0] * 40
         subjects[single_class['Subject']] = {'P': [], 'V': [], 'L': []}
 
+    # yak jadi ini perulangan intinya sih, buat nentuin posisi 1 gen
+    # perulangan mencari alokasi waktu untuk tiap 1 gen/KBM klo di kasus kita
+    # klo aslinya sih buat nentuin ruangan sama waktu
     for single_class in data:
-        new_single_class = single_class.copy()
 
+        # si new_single_class ini itu nanti jadi 1 gen baru
+        new_single_class = single_class.copy() 
+
+        # random cari kelas
+        # klo di kasus asli kodingan ini, berarti ini tu nyari ruangan yg kosong
+        # kyknya di kasus kita mah ga butuh ini
         classroom = random.choice(single_class['Classroom'])
-        day = random.randrange(0, 5)
-        if day == 4:
-            period = random.randrange(0, 7 - int(single_class['Length']))
-            print("Period", period)
-        else:
-            period = random.randrange(0, 9 - int(single_class['Length']))
-            print("Period", period)
-        new_single_class['Assigned_classroom'] = classroom
-        time = 8 * day + period
-        new_single_class['Assigned_time'] = time
 
-        for i in range(time, time + int(single_class['Length'])):
+        #####! random cari alokasi waktu (jam pelajaran) !######
+
+        # pertama cari hari dulu
+        day = random.randrange(0, 5)
+
+        # baru cari periode waktunya
+        if day == 4: # kalo jumat
+            period = random.randrange(0, 7 - int(single_class['Length']))
+        else:
+            # nb : jadi ges, menurut aku kita bisa otak atik di sini klo misal mau ngatasin masalah mapelnya kepotong istirahat
+            
+            period = random.randrange(0, 9 - int(single_class['Length']))
+            # kan sehari ada 8 jam pelajaran
+            # trus kita perlu tau kelas yg ini tuh berapa lama / berapa jam pelajaran, misal 3
+            # nah kita bakalan ngerandom antara 0 sampe (9 - 3)
+            # jadinya antara 0 sampe 6
+            # ya gitu lah intinya, biar 3 periode/jam pelajaran kepake gitu maksudnya
+
+        #####! selese !######
+
+        # nah ini mulai ngisi gen-nya ges, tapi kan yg kasus kita mah sebenernya cuma jam pelajarannya doang
+
+        new_single_class['Assigned_classroom'] = classroom # ya ini berarti kan kelas yg dirandom tadi tuh, ruangan klo di aslinya
+        
+        time = 8 * day + period # buat ngitung jam pelajaran-nya, paham lah ya ges
+        new_single_class['Assigned_time'] = time # masukin deh
+
+        # nah ini buat nandain yang tadi tuh
+        for i in range(time, time + int(single_class['Length'])): # kita tandain di index yang sesuai sama jam pelajaran tadi
+
+            # tandain klo gurunya ngajar di jam itu
             professors[new_single_class['Professor']][i] += 1
+            # tandain klo kelasnya (ruangan) kepake
             classrooms[classroom][i] += 1
+            # tandain klo kelas (yg kyk C1 gitu) lagi ada jam pelajaran
             for group in new_single_class['Group']:
                 groups[group][i] += 1
+
+        # ini juga buat nandain jadwal matkulnya
         subjects[new_single_class['Subject']][new_single_class['Type']].append(
             (time, new_single_class['Group']))
 
+        # masukin gen yg dibuat ke kromosom
+        # masukin 1 KBM ke jadwal
         new_data.append(new_single_class)
 
+    # mereturn kromosomnyaa
     return (new_data, professors, classrooms, groups, subjects)
-
+    # karena ngembaliin banyak data, si kromosom ini nanti bakalan jadi array (atau list ya, atau apalah gatau)
+    # chromosome[0] -> jadwal
+    # chromosome[1] -> jadwal guru (professors)
+    # chromosome[2] -> jadwal kelas (ruangan)
+    # chromosome[3] -> jadwal kelas (yg kyk C1)
+    # chromosome[4] -> jadwal mapel/matkul
 
 def write_data(data, path):
     with open(path, 'w') as write_file:
